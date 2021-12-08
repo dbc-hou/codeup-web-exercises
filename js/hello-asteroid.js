4fc20606b31e97bc6fe29e1f923476e7904f124f
dbc-hou <david.collins.hou@gmail.com>
2021-12-08 12:02:11 -0600

    adds hello-asteroid.js

--- js/hello-asteroid.js
+++ js/hello-asteroid.js
@@ -1,4 +1,5 @@
 var intro = "One of my favorite foods is: ";
 var favFood = "a calzone with spinach garlic, & mozzarella.";
-console.log(intro + favFood);
+var rationale = " For me, it's the ultimate comfort food."
+console.log(intro + favFood + rationale);
 
