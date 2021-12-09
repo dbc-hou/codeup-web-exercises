a735d50b247c81bacd70c730f1ab07e187b32512
dbc-hou <david.collins.hou@gmail.com>
2021-12-09 17:07:02 -0600

    added functions.js

--- js/hello-asteroid.js
+++ js/hello-asteroid.js
@@ -1,5 +1,5 @@
 var intro = "One of my favorite foods is";
 var favFood = "\n\ta calzone with: \n\t\tspinach, \n\t\tgarlic, \n\t\t& mozzarella.";
 var rationale = "\n\tFor me, it's the ultimate comfort food.";
-console.log(intro + favFood + rationale);
+console.log(Math.Round(Math.PI.toFixed(4),2);
 
