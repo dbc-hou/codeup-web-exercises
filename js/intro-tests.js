0ccd4177b854a6f5f4bab5facfa334fb3063ca8d
dbc-hou <david.collins.hou@gmail.com>
2021-12-15 11:30:44 -0600

    Testing Exercises

---
+++ js/intro-tests.js
@@ -0,0 +1,25 @@
+function count(stringyInput) {
+    if (stringyInput === null || stringyInput === undefined) {
+        return false;
+    }
+    let whatType = typeof stringyInput
+    if (whatType === "string") {
+        console.log("It's a string!");
+        console.log("The argument passed to this function is of type " + whatType + ", and it has " +
+            stringyInput.length + " characters.");
+        return stringyInput.length;
+    } else if (whatType === "number") {
+        console.log("The type of input is " + whatType + ".");
+        return false;
+    } else if (whatType === "boolean") {
+        console.log("The type of input is " + whatType + ".");
+        return false;
+    }
+}
+
+console.log("This string passed to the function has this many characters: " + count("The quick brown fox jumps over the lazy dog."));
+console.log("Do numerics work? " + count(23));
+console.log("Let's try true: " + count(true));
+console.log("Let's try false: " + count(false));
+console.log("Let's try a null value: " + count(null));
+console.log("What if we pass nothing to the parameter? " + count());
