2c413ecaa6cc7675fb0bd99ea606a9d665b9a657
dbc-hou <david.collins.hou@gmail.com>
2022-01-10 11:27:50 -0600

    html intro

--- js/date-object.js
+++ js/date-object.js
@@ -3,7 +3,7 @@ const myBirthday = new Date(1962,10,13);
 
 let e = d.getFullYear() + "-" + (d.getMonth()+1) + "-" + d.getDate();
 let f = d.getHours() + ":" + d.getMinutes() + ":" + d.getSeconds();
-let myAge = e - myBirthday;
+let myAge = d - myBirthday;
 alert("The current date is " + e);
 alert("The current time is " + f);
 alert("My current age is " + myAge);
