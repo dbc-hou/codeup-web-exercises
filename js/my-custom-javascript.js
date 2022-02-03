ceca09c643373b77a4418d078dd3f47fd7e7ad6d
dbc-hou <david.collins.hou@gmail.com>
2022-02-03 09:07:41 -0600

    jquery exercise step 2

--- js/my-custom-javascript.js
+++ js/my-custom-javascript.js
@@ -1,5 +1,9 @@
 "use strict";
 
-$(document).ready(function () {
-    alert("The DOM has finished loading.")
-})
+// $(document).ready(function () {
+//     let introContents = $('#introduction').text();
+//     alert(introContents);
+
+// $(document).ready(function () {
+//     $('.codeup').css('border:','1px solid #F00');
+// })
