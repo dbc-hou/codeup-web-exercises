2c01f5a0b14252efab33b32e391fd6fa77837748
dbc-hou <david.collins.hou@gmail.com>
2022-02-10 07:21:20 -0600

    open weather lecture

--- js/keys.js
+++ js/keys.js
@@ -1,1 +1,2 @@
-const DBC_MAPBOX_API_KEY = 'pk.eyJ1IjoiZGJjLWhvdSIsImEiOiJja3plZmlxamIzNzE0MnBuZjlyMzRqZjdhIn0.ombBI2IxUTjwKPLIE-3CQw'
+const DBC_MAPBOX_API_KEY = 'pk.eyJ1IjoiZGJjLWhvdSIsImEiOiJja3plZmlxamIzNzE0MnBuZjlyMzRqZjdhIn0.ombBI2IxUTjwKPLIE-3CQw';
+const DBC_OW_API_KEY = 'cdc858c141e5c1e6f2504f4f9c885baf';
