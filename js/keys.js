752fa8fe4f7dac44be2a46651d66944b932bafe7
dbc-hou <david.collins.hou@gmail.com>
2022-02-11 09:47:46 -0600

    weathermap project step 3

--- js/keys.js
+++ js/keys.js
@@ -1,2 +1,4 @@
 const DBC_MAPBOX_API_KEY = 'pk.eyJ1IjoiZGJjLWhvdSIsImEiOiJja3plZmlxamIzNzE0MnBuZjlyMzRqZjdhIn0.ombBI2IxUTjwKPLIE-3CQw';
 const DBC_OW_API_KEY = 'cdc858c141e5c1e6f2504f4f9c885baf';
+const MY_LNG_LAT_ARRAY = [-95.365,29.703];
+const MY_LNG_LAT_STRING = '-95.365,29.703';
