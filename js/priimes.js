51b75c1f6fbbc403654896e9e054255d8594b88b
dbc-hou <david.collins.hou@gmail.com>
2022-04-08 08:14:04 -0500

    added PrimesTo1000 function

--- js/priimes.js
+++ js/priimes.js
@@ -32,3 +32,18 @@ function First50Primes() {
 }
 
 //First50Primes();
+
+function PrimesTo1000() {
+    let i = 1
+    let j = 2
+    console.log("Here are the first 50 prime numbers!");
+    while (j<1000) {
+        if (isPrime(j)) {
+            console.log("# " + i + ": " + j);
+            i++;
+        }
+        j++
+    }
+}
+
+PrimesTo1000();
